5856008b6fcf351b904356adb8c8d209cef7eaf0
nguyenvanhuy2241988-star <nguyenvanhuy2241988@gmail.com>
2025-12-01 16:18:16 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Products from './components/Products';
